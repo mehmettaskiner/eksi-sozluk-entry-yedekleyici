6307480017e952172b90ba82637f6fa2e0301734
tanaydin sirin <tanaydin@fizy.com>
2014-08-27 13:36:36 +0300

    Squashed commit of the following:
    commit 3c1a9f46b8d89772e6db3b67bc624ae12a8c5a44
    Author: tanaydin sirin <tanaydin@fizy.com>
    Date:   Wed Aug 27 13:35:51 2014 +0300
    
        ek kontroller
        toplam entry'si 1 sayfa olan yazarda hata alınıyordu.
        readme dosyası formatlandı
    
    commit 3351a459b8764803667d62ae38efb11e386d1a59
    Author: tanaydin sirin <tanaydin@fizy.com>
    Date:   Wed Aug 27 11:54:36 2014 +0300
    
        bs4 yerine lxml parser
        komut satirindan parametre destegi
        yedek icinde entry tarihi de geliyor artik.

--- eksi_entry_yedekleyici.py
+++ eksi_entry_yedekleyici.py
@@ -1,49 +1,100 @@
 # -*- coding: utf-8 -*-
-import urllib2
-from bs4 import BeautifulSoup
+
+
+import timeit
 import sys
+import logging
+import urllib2
+import argparse
+from argparse import RawTextHelpFormatter
+from lxml import html
 
-def get_page_data(url):
-    request = urllib2.Request(url)
-    response = urllib2.urlopen(request)
-    data = response.read()
-    return data
+parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
+parser.add_argument('-y', '--yazar',
+                    help='yedegini cikarmak istediginiz yazarin nicki')
+parser.add_argument('-v', '--verbose',
+                    help='debug icin bisiler bas ekrana aslanim',
+                    action='store_true')
+args = parser.parse_args()
 
-def soup_data(data):
-    soup = BeautifulSoup(data)
-    return soup
+if args.verbose:
+    logging.basicConfig(level=logging.DEBUG)
 
-def start_fetching():
-    yazar_adi = raw_input("yazar adini girin: \n")
-    yazar_adi_replaced = yazar_adi.replace(" ", "-")
-    son_entry_url = "https://eksisozluk.com/biri/" + yazar_adi_replaced + "/son-entryleri"
 
-    filename = yazar_adi + " yedek.txt"
-    f = open(filename, "wb")
-    f.write("yedeklenen yazar: " + yazar_adi + "\n\n\n")
-    f.write("-----------------------------------------\n\n\n")
+class entry_yedek():
+    def __init__(self):
+        self.yazar_adi = ''
+        self.eksi_base_url = 'https://eksisozluk.com'
 
-    print 'kaydediliyor',
-    son_entryleri_soup = soup_data(get_page_data(son_entry_url))
+    def stringify_children(self, node):
+        if node is None:
+            return ''
 
-    sayfa_sayisi = int(son_entryleri_soup.find("div", attrs={'class': 'pager'})['data-pagecount'])
-    for i in range(1, sayfa_sayisi + 1):
-        print '\b.',
-        sayfa_url = son_entry_url + "?p=" + str(i)
-        sayfa_soup = soup_data(get_page_data(sayfa_url))
-        
-        entry_urls = sayfa_soup.find_all("ul", attrs={'class': 'topic-list'})[1]
-        a = entry_urls.find_all("a") # all a tags under ul tag
-        for link in a:
-            entry_url = "https://eksisozluk.com" + str(link['href'])
-            entry_soup = soup_data(get_page_data(entry_url))
+        s = node.text
+        if s is None:
+            s = ''
+        for child in node:
+            s += html.tostring(child, encoding='unicode')
+        return s
 
-            baslik = entry_soup.find("h1", attrs={'id': 'title'})['data-title'].encode('utf-8')
-            entry = entry_soup.find("div", attrs={'class': 'content', 'itemprop': 'commentText'}).text.encode('utf-8')
-            f.write(baslik + ":\n")
-            f.write(entry + "\n\n")
-        sys.stdout.flush()
+    def page_tree(self, url):
+        try:
+            request = urllib2.Request(url)
+            page_data = urllib2.urlopen(request).read()
+            return html.fromstring(page_data)
+        except urllib2.HTTPError:
+            print 'bu yazari (%s) bulamadim...' % self.yazar_adi
+            exit()
 
-    f.close()
+    def start_fetching(self):
+        yazar_adi_replaced = yazar_adi.replace(" ", "-")
+        son_entry_url = self.eksi_base_url + "/biri/" + yazar_adi_replaced + "/son-entryleri"
+        page_info = self.page_tree(son_entry_url).find(".//div[@class='pager']")
+        if page_info is None:
+            sayfa_sayisi = 1
+        else:
+            sayfa_sayisi = int(page_info.get('data-pagecount'))
 
-start_fetching()
+        logging.info("yedeklenen yazar: " + yazar_adi)
+        filename = yazar_adi + "_yedek.txt"
+        f = open(filename, "wb")
+
+        logging.info("kaydediliyor")
+        logging.info("toplam sayfa sayisi : %s" % sayfa_sayisi)
+
+        for i in range(1, sayfa_sayisi + 1):
+            logging.info("islenilen sayfa : %s " % i)
+            sayfa_url = son_entry_url + "?p=" + str(i)
+            a = self.page_tree(sayfa_url).findall(".//ul[@class='topic-list']//a")
+            for link in a:
+                logging.info("baslik : %s - link : %s" % (link.text.strip(), link.get('href')))
+                entry_tree = self.page_tree(self.eksi_base_url + str(link.get('href')))
+                if entry_tree is not None:
+                    baslik = entry_tree.find(".//h1[@id='title']").get('data-title').encode('utf-8').strip()
+                    entry = self.stringify_children(
+                        entry_tree.find(".//div[@class='content'][@itemprop='commentText']")).encode('utf-8').strip()
+                    tarih = entry_tree.find(".//time").text.strip()
+
+                    f.write("%s\n%s\n%s\n\n" % (baslik, entry, tarih))
+
+            sys.stdout.flush()
+
+        f.close()
+
+
+if __name__ == "__main__":
+    start = timeit.default_timer()
+    if args.yazar is None:
+        yazar_adi = raw_input("yazar adini girin: ")
+    else:
+        yazar_adi = args.yazar
+    if yazar_adi:
+        ey = entry_yedek()
+        ey.yazar_adi = yazar_adi
+        ey.start_fetching()
+    else:
+        print 'o isim buraya yazilacak'
+
+    stop = timeit.default_timer()
+
+    print 'Toplam %s saniye surdu tum islem... ' % int(stop - start)
